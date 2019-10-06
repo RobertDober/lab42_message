42ef9e2d6bb2b8e1d0471368f1b5b87fb29dcb1c
RobertDober <robert.dober@gmail.com>
2019-10-06 14:53:10 +0200

    Working twds Iteration 0 with all tools integration
      - travis with tests and coveralls

--- lib/lab42/message.ex
+++ lib/lab42/message.ex
@@ -15,11 +15,22 @@ defmodule Lab42.Message do
   @type severity_t :: :debug | :info | :warning | :error | :critical | :fatal
   @type location_t :: any()
   @type t :: %__MODULE__{location: location_t(), message: String.t, severity: severity_t()}
+  @type ts :: list(t)
   @type message_t :: {severity_t(), String.t, location_t()}
   @type result_t :: {:ok|:error, any(), list(message_t)}
 
   
-  for severity <- [:debug] do
+  severities = ~w(debug info warning error critical fatal)a
+  @severities severities
+
+  for {severity, index} <- severities |> Enum.zip(Stream.iterate(1, &(&1+1))) do
+    @doc """
+    Create a message with severity :#{severity} and add in front of other messages
+
+        iex(#{index})> Lab42.Message.add_#{severity}([], "Just a #{severity} message", {1, 3})
+        [%Lab42.Message{message: "Just a #{severity} message", severity: :#{severity}, location: {1, 3}}]
+    """
+    @spec unquote( :"add_#{severity}" )(ts(), String.t, any()) :: ts()
     def unquote(:"add_#{severity}")(messages, message, location) do
       [struct(__MODULE__, severity: unquote(severity), location: location, message: message) | messages]
     end
--- test/lab42/message/add_message_test.exs
+++ test/lab42/message/add_message_test.exs
@@ -1,19 +1,6 @@
 defmodule Lab42.Message.AddMessageTest do
   use ExUnit.Case
 
-  import Lab42.Message
-
-  describe "add_*" do
-    test "debug" do
-      messages = add_debug([], "debug message", 42)
-      expected = msg(message: "debug message", location: 42, severity: :debug)
-
-      assert messages == expected
-    end
-  end
-
-  defp msg(options) do
-    [ struct(Lab42.Message, options) ]
-  end
+  doctest Lab42.Message
   
 end
